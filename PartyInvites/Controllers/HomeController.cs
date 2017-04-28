225e33a72eaed134c618e033cff7bc15027e0697
juhma <juhma@ebay.com>
2017-04-28 11:05:40 +0900

    first commit at Edu Session1

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+	public class HomeController : Controller
+	{
+		
+		// GET: Home
+		//private int value1;
+		public ViewResult Index()
+		{
+			//this.value1 = 10;
+			//return View();
+
+			//0426 실습 1
+			int hour = DateTime.Now.Hour;
+			this.ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
+			return this.View();
+		}
+
+		[HttpGet]
+		public ViewResult RsvpForm()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse guestResponse)
+		{
+			if (ModelState.IsValid)
+				return View("Thanks", guestResponse);
+			else
+				return View();
+		}
+	}
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+	public class GuestResponse
+	{
+		[Required(ErrorMessage ="이름 입력 필수")]
+		public string Name { get; set; }
+		public string Email { get; set; }
+		[Required(ErrorMessage = "전화번호 입력 필수")]
+		public string Phone { get; set; }
+		public bool? WillAttend { get; set; }
+	}
+}
