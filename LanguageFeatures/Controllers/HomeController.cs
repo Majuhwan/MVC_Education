c438ae24d11873d71231408f2a9388a003f46ddc
juhma <juhma@ebay.com>
2017-04-28 11:06:13 +0900

    first commit at Edu session2

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+	public class HomeController : Controller
+	{
+		// GET: Home
+		public string Index()
+		{
+			return "return String";
+		}
+		public ViewResult AutoProperty()
+		{
+			Product myProduct = new Product();
+
+			myProduct.Name = "kayak";
+
+			string productName = myProduct.Name;
+
+			return View("Result", (object)String.Format("Product name; {0}", productName));
+		}
+	}
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+	public class Product
+	{
+		//private string _name;
+		
+		//public string Name
+		//{
+		//	get { return _name;}
+		//	set { _name = value; }
+		//}
+		
+		public string Name { get; set; }
+	}
+}
